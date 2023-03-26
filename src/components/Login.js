552ed6c5965411ec0bf47a135cf7aa903f0834d0
Felicia Hoeft <fhoeft92615@gmail.com>
2023-03-25 19:07:43 -0500

    added local storage support for logging in

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from "react-bootstrap";
 import { Form, FormGroup, Label, Input } from 'reactstrap';
 import handleSubmit from './handlesubmit';
+import { findAllUsers } from '../services/users';
 
 function LoginPage() {
+    // This is for handling local storage
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            const res = await findAllUsers()
+            setUsers([...res])
+            var memberName = localStorage.getItem("username");
+            var memberPass = localStorage.getItem("password");
+            // console.log(memberName)
+            // console.log(memberPass)
+            // console.log(users)
+            setIsLoggedIn(users.some(user => user.username === memberName &&
+                user.password === memberPass))
+
+        }
+        fetchData().catch(console.error)
+
+    }, [users])
+
+    // Useful variables 
+    const [showLogin, setShowLogin] = useState(false);
     const [showRegister, setShowRegister] = useState(false);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    // Useful functions
     const onChangeUsername = (e) => {
         setUsername(e.target.value);
     };
@@ -27,10 +52,29 @@ function LoginPage() {
         setPassword("");
         setShowRegister(false);
     };
+    const onLoginSubmit = (e) => {
+        e.preventDefault()
+        if (users.some(user => user.username === username && user.password === password)) {
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+            setIsLoggedIn(true)
+        } else {
+            setShowLogin(false);
+        }
+    };
+    const handleLogout = (e) => {
+        e.preventDefault()
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+        setIsLoggedIn(false);
+    };
     return (
         <div className="loginPage">
             {isLoggedIn ? (
-                <div>This is logged in!</div>
+                <div>Hello {localStorage.getItem("username")}!
+                    <Button as="button" type="submit" value="Logout"
+                        onClick={handleLogout}>Logout</Button>
+                </div>
             ) : (
                 <div>
 
@@ -62,10 +106,43 @@ function LoginPage() {
                             </Button>
                         </Form>
                     ) : (
-                        <div><Button as="button" type="submit" value="Login">Login</Button>
-                            <Button as="button" type="reset" value="Register"
-                                onClick={() => setShowRegister(true)}>Register</Button></div>
-
+                        <div>
+                            {showLogin ? (
+                                <Form onSubmit={onLoginSubmit}>
+                                    <FormGroup>
+                                        <Label for="username">Username</Label>
+                                        <Input
+                                            type="text"
+                                            placeholder="Username"
+                                            value={username}
+                                            onChange={onChangeUsername}
+                                            required
+                                        />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="password">Password</Label>
+                                        <Input
+                                            className="password"
+                                            type="text"
+                                            placeholder="Password"
+                                            value={password}
+                                            onChange={onChangePassword}
+                                            required
+                                        />
+                                    </FormGroup>
+                                    <Button type="submit" id="loginSubmit" style={{ background: "#fbb040" }}>
+                                        Login
+                                    </Button>
+                                </Form>
+                            ) : (
+                                <div>
+                                    <Button as="button" type="submit" value="Login"
+                                        onClick={() => setShowLogin(true)}>Login</Button>
+                                    <Button as="button" type="reset" value="Register"
+                                        onClick={() => setShowRegister(true)}>Register</Button>
+                                </div>
+                            )}
+                        </div>
                     )}
 
                 </div>
