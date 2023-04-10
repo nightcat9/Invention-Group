78d7ea0846ba95d58292acc9fcaea93beb0d1032
Felicia Hoeft <fhoeft92615@gmail.com>
2023-04-10 18:17:26 -0500

    added /home to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -48,7 +48,7 @@ function Navigation() {
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-                <Nav.Link as={Link} to={"home"}>Home</Nav.Link>
+                <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
                 <Nav.Link as={Link} to={"/about"}>About Us</Nav.Link>
                 <Nav.Link as={Link} to={"/expertise"}>Expertise</Nav.Link>
                 <Nav.Link as={Link} to={"/process"}>Process</Nav.Link>
