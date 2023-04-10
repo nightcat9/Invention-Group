f9cb9401c39555ba38c5fc2c36ea490a9cee624a
Felicia Hoeft <fhoeft92615@gmail.com>
2023-04-09 22:40:51 -0500

    trying to get footer link working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,6 @@ function Footer() {
                     <Row className="justify-content-md-center">
                         <Col md={12} lg={2}>
                             <ul>
-
                                 <Link to={"/home"}><li className='footer-nav'><a href={"/home"}>Home</a></li></Link> 
                                 <li className='footer-nav'><a href={"/about"}>About Us</a></li>
                                 <li className='footer-nav'><a href={"/expertise"}>Expertise</a></li>
